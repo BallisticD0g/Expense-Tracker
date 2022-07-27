5be3cef652b37e5364a74b2b7b0ae229de9bc842
Mikel <90412985+BallisticD0g@users.noreply.github.com>
2022-07-27 19:39:32 -0400

    Update
    functionality fully implemented  aplicati0on should work as intended if not contact asap

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,18 +8,48 @@ import { Component } from '@angular/core';
 export class Tab1Page {
   numbInput;
   newNum;
+  percentage;
+  prog;
+  originalNum;
   constructor() {}
 
   btnNew(){
-    this.newNum = this.numbInput;
+     this.newNum = this.numbInput;
+     this.originalNum = this.newNum = this.numbInput;
+     this.prog = 1;
+     this.percentage = 100;
   }
 
   btnAdd(){
-    this.newNum = this.numbInput + this.newNum;
+    if((this.newNum = this.numbInput + this.newNum) >= this.originalNum){
+      this.prog = 1;
+      this.percentage = 100;
+    } else {
+      const addDec = this.numbInput / this.newNum;
+      const addRoundDec = Math.floor(addDec * 10) / 10;
+      this.percentage = this.percentage + addRoundDec * 100;
+      this.prog = this.prog + addRoundDec;
+
+      console.log('AddDec', addDec);
+      console.log('AddDecRound', addRoundDec);
+      console.log('AddPer', this.percentage);
+    }
+
   }
 
   btnMin(){
+    //substacts from dollar amount
+     //calculate the decamal value then round it for use.
+    const dec = this.numbInput / this.newNum;
+    const roundDec = Math.floor(dec * 10) / 10;
+    //substacts from percentage amount
+    this.prog = this.prog - roundDec;
+    //calculatet the percentage
+    this.percentage = this.prog * 100;
     this.newNum = this.newNum - this.numbInput;
+    console.log('SUbDec', roundDec);
+    console.log('SUBPer', this.percentage);
   }
+
 
 }
